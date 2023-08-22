efcedb08256e1746e2dabf40dc091061597dda1e
Hugo S Juarez P <hsebastianjuarezp@gmail.com>
2023-08-22 09:14:09 +0100

    added props to Cards

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Card = ()=> {
+const Card = ({id, name, username, email})=> {
     return (
         <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
             <img alt="robot" src="https://robohash.org/test?200x200" />
             <div>
-                <h2>Jane Doe</h2>
-                <p>jane.doe@gmail.com</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Card from './Card';
+import {robots} from './robots';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
-    <Card />
-    <Card />
-    <Card />
+    <Card id={robots[0].id} name={robots[0].name} username={robots[0].username} email={robots[0].email} />
+    <Card id={robots[1].id} name={robots[1].name} username={robots[1].username} email={robots[1].email} />
+    <Card id={robots[2].id} name={robots[2].name} username={robots[2].username} email={robots[2].email} />
   </div>
 );
 
