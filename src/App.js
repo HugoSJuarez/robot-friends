c884c5d454c95dd110a23065cb1e15a0e9067d50
Hugo S Juarez P <hsebastianjuarezp@gmail.com>
2023-08-23 10:15:21 +0100

    added state function

--- src/App.js
+++ src/App.js
@@ -9,14 +9,23 @@ class App extends Component {
         this.state = {
             robots: robots,
             searchfield: ''
-        }
+        };
     }
+
+    onSearchChange = (e)=>{
+        this.setState({searchfield: e.target.value})
+    }
+
     render(){
+        const filteredRobots = this.state.robots.filter(robot=>{
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        });
+
         return(
             <div className="tc">
                 <h1>RoboFriends</h1>
-                <SearchBox />
-                <CardList list={robots}/>
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList list={filteredRobots}/>
             </div>
         );
     }
