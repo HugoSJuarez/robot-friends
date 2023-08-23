76c26d783a8ba2ae09dea1787d7bd9e3a227689a
Hugo S Juarez P <hsebastianjuarezp@gmail.com>
2023-08-23 08:45:36 +0100

    readded App component in index

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Card from './Card';
-import {robots} from './robots';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div>
-    <Card id={robots[0].id} name={robots[0].name} username={robots[0].username} email={robots[0].email} />
-    <Card id={robots[1].id} name={robots[1].name} username={robots[1].username} email={robots[1].email} />
-    <Card id={robots[2].id} name={robots[2].name} username={robots[2].username} email={robots[2].email} />
-  </div>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
