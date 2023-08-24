07b9874304ad274d9e2c2258c3281465ac29909a
Hugo S Juarez P <hsebastianjuarezp@gmail.com>
2023-08-24 09:48:34 +0200

    cleaned code and added folder structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
-import SearchBox from "./SearchBox";
-import Scroll from "./Scroll";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
 import './App.css';
 
 class App extends Component {
@@ -16,7 +16,7 @@ class App extends Component {
     componentDidMount(){
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
-            .then(users => this.setState({robots:users}));
+            .then(users => this.setState({robots: users}));
     }
 
     onSearchChange = (e)=>{
@@ -24,10 +24,14 @@ class App extends Component {
     }
 
     render(){
-        const filteredRobots = this.state.robots.filter(robot=>{
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const {robots, searchfield} = this.state;
+        const filteredRobots = robots.filter(robot=>{
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         });
 
+        if(robots.length===0){
+            return <h1>Loading</h1>
+        }
         return(
             <div className="tc">
                 <h1 className="f1">RoboFriends</h1>
